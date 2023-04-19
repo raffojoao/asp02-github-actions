3c81da7824b29b2c3a88f01ab3e99d2c2f4834e2
João Raffo <jomf@gft.com>
2023-04-19 16:43:55 -0300

    test bot with env

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+!/bin/sh
 
 # Get the token from Travis environment vars and build the bot URL:
 BOT_URL="https://api.telegram.org/bot5616652514:AAGJrfbzOTiai5v8kkHinfPTfnrwiW0fDw4/sendMessage"
@@ -17,7 +17,7 @@ fi
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
 send_msg () {
-    echo ${{ secrets.DOCKERHUB_USERNAME }}
+    echo $telegram_bot_url
     # curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
     #     -d text="$1" -d parse_mode=${PARSE_MODE}
 }
@@ -25,13 +25,13 @@ send_msg () {
 # Send message to the bot with some pertinent details about the job
 # Note that for Markdown, you need to escape any backtick (inline-code)
 # characters, since they're reserved in bash
-send_msg "
--------------------------------------
-Hello João! Github build *${build_status}!*
-\`Repository:  ${TRAVIS_REPO_SLUG}\`
-\`Branch:      ${TRAVIS_BRANCH}\`
-*Commit Msg:*
-${TRAVIS_COMMIT_MESSAGE}
-[Job Log here](${TRAVIS_JOB_WEB_URL})
---------------------------------------
-"
+# send_msg "
+# -------------------------------------
+# Hello João! Github build *${build_status}!*
+# \`Repository:  ${TRAVIS_REPO_SLUG}\`
+# \`Branch:      ${TRAVIS_BRANCH}\`
+# *Commit Msg:*
+# ${TRAVIS_COMMIT_MESSAGE}
+# [Job Log here](${TRAVIS_JOB_WEB_URL})
+# --------------------------------------
+# "
